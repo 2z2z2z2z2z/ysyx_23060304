93bf4832206a7fefb5497dafba1897ec28105272
tracer-ysyx <tracer@ysyx.org>
2024-04-18 15:10:27 +0800

    > compile NEMU 2023234344 Zheng Tianhao Linux z 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:10:27 up 1 day, 12:07,  3 users,  load average: 0.51, 0.48, 0.52

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -79,8 +79,8 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  {"si [N]", "Execute N instructions and pause, default 1", cmd_si},
-  {"info SUBCMD", "info r: Print register; info w: Print watchpoint", cmd_info},
+  {"si", "si [N]: Execute N instructions and pause, default 1", cmd_si},
+  {"info", "info r: Print register; info w: Print watchpoint", cmd_info},
   {"x N EXPR", "Use EXPR as the starting memory address, output N 4 bytes in hex", cmd_mem},
   /* TODO: Add more commands */
 
