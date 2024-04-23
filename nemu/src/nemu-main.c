b0068332b76d97548f139667c7534a6560af9e61
tracer-ysyx <tracer@ysyx.org>
2024-04-23 22:41:04 +0800

    > compile NEMU 2023234344 Zheng Tianhao Linux z 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:41:04 up 2 days,  2:24,  4 users,  load average: 0.48, 0.46, 0.60

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
   if (file == NULL){
     printf("wrong");
   }
-  while (fscanf(file, "%ud %[^\n]", &res, expr)) {
+  while (fscanf(file, "%ud %[^\\n]", &res, expr)) {
     printf("res: %d %s\n", res, expr);
   }
 
